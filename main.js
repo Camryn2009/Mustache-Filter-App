35af65783d6b7c76c426c2b3e1952ab80d3a32aa
Camryn2009 <89714877+Camryn2009@users.noreply.github.com>
2022-07-22 20:49:49 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-function preload(){
+noseX=0;
+noseY=0;
 
+function preload(){
+clown_nose=loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
 }
 
 function setup(){
@@ -20,6 +23,7 @@ function modelLoaded(){
 
 function draw(){
 image(video, 0, 0, 300, 300);
+image(clown_nose,noseX-25, noseY-5, 50, 50);
 }
 
 function take_snapshot(){
@@ -31,5 +35,7 @@ function gotPoses(results){
         console.log(results);
         console.log("nose x =" + results[0].pose.nose.x);
         console.log("nose y =" + results[0].pose.nose.y);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
     }
 }
